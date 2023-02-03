72b16155d84870a503655e93edf163ebee97d06b
abdelbenamara <abdelb.contact@gmail.com>
2023-02-03 00:40:04 +0000

    Updated ft_vdprintf

--- ft_vdprintf.c
+++ ft_vdprintf.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 01:02:55 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/01 21:53:05 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/02/02 01:00:00 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	ft_vdprintf(int fd, const char *format, va_list ap)
 {
+	int	nb;
+
 	(void)ap;
+	nb = 0;
 	while (*format)
 	{
 		ft_putchar_fd(*format, fd);
 		++format;
+		++nb;
 	}
-	return (0);
+	return (nb);
 }
