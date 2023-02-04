6ec01241fc9856c0493018af020e9961514c2b38
abdelbenamara <abdelb.contact@gmail.com>
2023-02-04 23:38:52 +0000

    Updated ft_vdprintf

--- ft_vdprintf.c
+++ ft_vdprintf.c
@@ -6,23 +6,35 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 01:02:55 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/02 01:00:00 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/02/04 23:02:09 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "_conversion.h"
 #include "libftprintf.h"
 
 int	ft_vdprintf(int fd, const char *format, va_list ap)
 {
 	int	nb;
+	int	s_len;
 
-	(void)ap;
 	nb = 0;
 	while (*format)
 	{
-		ft_putchar_fd(*format, fd);
+		if (*format != '%')
+		{
+			ft_putchar_fd(*format, fd);
+			++nb;
+		}
+		else
+		{
+			s_len = ft_specification_len(format + 1);
+			if (!s_len)
+				return (-1);
+			nb += ft_putconversion_fd(format[s_len], format, ap, fd);
+			format += s_len;
+		}
 		++format;
-		++nb;
 	}
 	return (nb);
 }
