a1036aa9245cebdf0c7f7d5566c704c85999ff0c
abdelbenamara <abdelb.contact@gmail.com>
2023-02-04 23:38:48 +0000

    Updated libftprintf

--- libftprintf.h
+++ libftprintf.h
@@ -6,15 +6,15 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 00:34:14 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/01 21:56:00 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/02/04 12:27:11 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include "libft.h"
 # include <stdarg.h>
+# include "libft.h"
 
 int	ft_vdprintf(int fd, const char *format, va_list ap);
 int	ft_vprintf(const char *format, va_list ap);
