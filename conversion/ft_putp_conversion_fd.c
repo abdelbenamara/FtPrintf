a698887aa3b9959fca5328576de9707c42e8d31c
abdelbenamara <abdelb.contact@gmail.com>
2023-02-04 23:36:06 +0000

    Added ft_putp_conversion_fd

---
+++ conversion/ft_putp_conversion_fd.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putp_conversion_fd.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 16:00:40 by abenamar          #+#    #+#             */
+/*   Updated: 2023/02/04 21:16:16 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "_conversion.h"
+
+int	ft_putp_conversion_fd(const char *format, va_list ap, int fd)
+{
+	(void)format;
+	(void)ap;
+	(void)fd;
+	return (0);
+}
