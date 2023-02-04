ed4aa6aa99b1a14e86c4921a67e6f744447e445c
abdelbenamara <abdelb.contact@gmail.com>
2023-02-04 23:36:44 +0000

    Added ft_putupper_x_conversion_fd

---
+++ conversion/ft_putupper_x_conversion_fd.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putupper_x_conversion_fd.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 16:03:24 by abenamar          #+#    #+#             */
+/*   Updated: 2023/02/04 21:16:49 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "_conversion.h"
+
+int	ft_putupper_x_conversion_fd(const char *format, va_list ap, int fd)
+{
+	(void)format;
+	(void)ap;
+	(void)fd;
+	return (0);
+}
