ab1259cf2e82c6a1ff973e960f2b35952422b368
abdelbenamara <abdelb.contact@gmail.com>
2023-02-07 02:01:21 +0000

    Updated ft_put_x_conversion_fd

--- conversion/ft_put_x_conversion_fd.c
+++ conversion/ft_put_x_conversion_fd.c
@@ -1,21 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putx_conversion_fd.c                            :+:      :+:    :+:   */
+/*   ft_put_x_conversion_fd.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:03:06 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/04 23:02:32 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/02/07 01:22:51 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "_conversion.h"
+#include "utils/libftprintf_utils.h"
+#include "conversion/libftprintf_conversion.h"
 
-int	ft_putx_conversion_fd(const char *format, va_list ap, int fd)
+int	ft_put_x_conversion_fd(const char *format, va_list ap, int fd)
 {
+	int				nb;
+	unsigned int	x;
+	char			*a;
+
 	(void)format;
-	(void)ap;
-	(void)fd;
-	return (0);
+	x = va_arg(ap, unsigned int);
+	a = ft_uitoa_base(x, "0123456789abcdef");
+	nb = 0;
+	while (a[nb])
+	{
+		ft_putchar_fd(a[nb], fd);
+		++nb;
+	}
+	free(a);
+	return (nb);
 }
