5df255860102cf095ce504d0661307e85d97c2d5
abdelbenamara <abdelb.contact@gmail.com>
2023-02-07 01:57:46 +0000

    Updated ft_put_c_conversion_fd

--- conversion/ft_put_c_conversion_fd.c
+++ conversion/ft_put_c_conversion_fd.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putc_conversion_fd.c                            :+:      :+:    :+:   */
+/*   ft_put_c_conversion_fd.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 13:35:57 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/04 21:15:33 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/02/07 01:21:09 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "_conversion.h"
+#include "conversion/libftprintf_conversion.h"
 
-int	ft_putc_conversion_fd(const char *format, va_list ap, int fd)
+int	ft_put_c_conversion_fd(const char *format, va_list ap, int fd)
 {
 	int		nb;
 	char	c;
