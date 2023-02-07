ccb6ec7490b549815c94c30354e8be0768fbb814
abdelbenamara <abdelb.contact@gmail.com>
2023-02-07 02:00:40 +0000

    Added libftprintf_utils

---
+++ utils/libftprintf_utils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf_utils.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 00:49:15 by abenamar          #+#    #+#             */
+/*   Updated: 2023/02/07 01:14:28 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_UTILS_H
+# define LIBFTPRINTF_UTILS_H
+
+# include "libftprintf.h"
+
+char	*ft_uitoa_base(unsigned int n, const char *base);
+
+#endif
