01e9b6ab6f94def8ec65590cf2586ae7e8a7d928
abdelbenamara <abdelb.contact@gmail.com>
2023-04-01 23:25:13 +0100

    Updated ft_put_percent_conversion_fd

--- conversion/ft_put_percent_conversion_fd.c
+++ conversion/ft_put_percent_conversion_fd.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:03:41 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/01 15:39:13 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/01 18:44:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conversion/libftprintf_conversion.h"
 
-int	ft_put_percent_conversion_fd(const char *format, va_list ap, int fd)
+int	ft_put_percent_conversion_fd(const char *format, va_list *ap, int fd)
 {
 	(void)format;
 	(void)ap;
