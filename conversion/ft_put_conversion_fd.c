4d34bad7a3c162f30e3f9050b803880f87af4cb9
abdelbenamara <abdelb.contact@gmail.com>
2023-04-01 16:35:39 +0100

    Updated ft_put_conversion_fd

--- conversion/ft_put_conversion_fd.c
+++ conversion/ft_put_conversion_fd.c
@@ -6,25 +6,34 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 13:56:33 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/07 01:34:14 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/01 16:09:05 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "specification/libftprintf_specification.h"
 #include "conversion/libftprintf_conversion.h"
 
-int	ft_put_conversion_fd(size_t s_len, const char *fmt, va_list ap, int fd)
+int	ft_put_conversion_fd(size_t s_len, const char *format, va_list ap, int fd)
 {
-	const t_conv	*conversions = ft_init_conversions();
-	const char		*specifiers = SPECIFIERS;
-	uint8_t			i;
+	static int	(*conversions[])(const char *, va_list, int) = {
+		ft_put_c_conversion_fd,
+		ft_put_s_conversion_fd,
+		ft_put_p_conversion_fd,
+		ft_put_d_conversion_fd,
+		ft_put_i_conversion_fd,
+		ft_put_u_conversion_fd,
+		ft_put_x_conversion_fd,
+		ft_put_upper_x_conversion_fd,
+		ft_put_percent_conversion_fd
+	};
+	uint8_t		i;
 
-	if (!s_len)
-		return (-1);
 	i = 0;
-	while (specifiers[i] != fmt[s_len])
+	while (SPECIFIERS[i] != format[s_len])
+	{
 		++i;
-	if (i == CONVERSION_AMOUNT)
-		return (-1);
-	return (conversions[i](fmt, ap, fd));
+		if (i == 9)
+			return (ft_put_no_conversion_fd(s_len, format, fd));
+	}
+	return (conversions[i](format, ap, fd));
 }
