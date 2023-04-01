c9222759ffd392f91f611bae400bd029b117709c
abdelbenamara <abdelb.contact@gmail.com>
2023-04-01 23:25:23 +0100

    Updated ft_put_u_conversion_fd

--- conversion/ft_put_u_conversion_fd.c
+++ conversion/ft_put_u_conversion_fd.c
@@ -6,21 +6,21 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:02:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/07 01:22:39 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/01 18:45:56 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils/libftprintf_utils.h"
 #include "conversion/libftprintf_conversion.h"
 
-int	ft_put_u_conversion_fd(const char *format, va_list ap, int fd)
+int	ft_put_u_conversion_fd(const char *format, va_list *ap, int fd)
 {
 	int				nb;
 	unsigned int	u;
 	char			*a;
 
 	(void)format;
-	u = va_arg(ap, unsigned int);
+	u = va_arg(*ap, unsigned int);
 	a = ft_uitoa_base(u, "0123456789");
 	nb = 0;
 	while (a[nb])
