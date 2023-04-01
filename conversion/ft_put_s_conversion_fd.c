eff4745c6ae4101c0d70eeebe85451df9d3321a1
abdelbenamara <abdelb.contact@gmail.com>
2023-04-01 23:25:17 +0100

    Updated ft_put_s_conversion_fd

--- conversion/ft_put_s_conversion_fd.c
+++ conversion/ft_put_s_conversion_fd.c
@@ -6,25 +6,33 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 15:58:19 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/07 01:22:19 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/01 23:09:35 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conversion/libftprintf_conversion.h"
 
-int	ft_put_s_conversion_fd(const char *format, va_list ap, int fd)
+int	ft_put_s_conversion_fd(const char *format, va_list *ap, int fd)
 {
 	int		nb;
 	char	*s;
 
 	(void)format;
-	nb = 0;
-	s = va_arg(ap, char *);
-	while (*s)
+	s = va_arg(*ap, char *);
+	if (!s)
 	{
-		ft_putchar_fd(*s, fd);
-		++s;
-		++nb;
+		ft_putstr_fd("(null)", fd);
+		nb = 6;
+	}
+	else
+	{
+		nb = 0;
+		while (*s)
+		{
+			ft_putchar_fd(*s, fd);
+			++s;
+			++nb;
+		}
 	}
 	return (nb);
 }
