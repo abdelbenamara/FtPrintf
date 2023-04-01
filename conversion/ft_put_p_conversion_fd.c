004a31b1fdec35fafcc03129b3f1bc052bf14836
abdelbenamara <abdelb.contact@gmail.com>
2023-04-01 23:25:08 +0100

    Updated ft_put_p_conversion_fd

--- conversion/ft_put_p_conversion_fd.c
+++ conversion/ft_put_p_conversion_fd.c
@@ -6,21 +6,21 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:00:40 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/01 13:42:09 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/01 18:45:40 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils/libftprintf_utils.h"
 #include "conversion/libftprintf_conversion.h"
 
-int	ft_put_p_conversion_fd(const char *format, va_list ap, int fd)
+int	ft_put_p_conversion_fd(const char *format, va_list *ap, int fd)
 {
 	int		nb;
 	void	*p;
 	char	*a;
 
 	(void)format;
-	p = va_arg(ap, void *);
+	p = va_arg(*ap, void *);
 	if (!p)
 	{
 		ft_putstr_fd("(nil)", fd);
