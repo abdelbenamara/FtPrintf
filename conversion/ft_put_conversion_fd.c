77ba4ceb215cdc76c07e85b868b001f388cf32cc
abdelbenamara <abdelb.contact@gmail.com>
2023-04-01 23:25:50 +0100

    Updated ft_put_conversion_fd

--- conversion/ft_put_conversion_fd.c
+++ conversion/ft_put_conversion_fd.c
@@ -6,16 +6,16 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 13:56:33 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/01 16:09:05 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/01 18:46:53 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "specification/libftprintf_specification.h"
 #include "conversion/libftprintf_conversion.h"
 
-int	ft_put_conversion_fd(size_t s_len, const char *format, va_list ap, int fd)
+int	ft_put_conversion_fd(size_t s_len, const char *format, va_list *ap, int fd)
 {
-	static int	(*conversions[])(const char *, va_list, int) = {
+	static int	(*conversions[])(const char *, va_list *, int) = {
 		ft_put_c_conversion_fd,
 		ft_put_s_conversion_fd,
 		ft_put_p_conversion_fd,
