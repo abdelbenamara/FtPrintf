8c8f7705a4fc43458248aab273746d4e73e978ca
abdelbenamara <abdelb.contact@gmail.com>
2023-04-15 14:11:05 +0100

    Updated libftprintf_conversion_specifiers

--- conversion/specifiers/libftprintf_conversion_specifiers.h
+++ conversion/specifiers/libftprintf_conversion_specifiers.h
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf_specification.h                        :+:      :+:    :+:   */
+/*   libftprintf_conversion_specifiers.h                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/07 00:41:59 by abenamar          #+#    #+#             */
-/*   Updated: 2023/02/07 01:14:51 by abenamar         ###   ########.fr       */
+/*   Created: 2023/04/15 12:08:10 by abenamar          #+#    #+#             */
+/*   Updated: 2023/04/15 12:10:09 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_SPECIFICATION_H
-# define LIBFTPRINTF_SPECIFICATION_H
+#ifndef LIBFTPRINTF_CONVERSION_SPECIFIERS_H
+# define LIBFTPRINTF_CONVERSION_SPECIFIERS_H
 
-# define SPECIFIERS	"cspdiuxX%"
+# define SPECIFIERS "cspdiuxX%"
 
 # include "libftprintf.h"
 
+typedef struct s_flags
+{
+	uint8_t	alternate_form;
+	size_t	specifier_len;
+}	t_flags;
+
 uint8_t	ft_is_specifier(char c);
-size_t	ft_specification_len(const char *format);
+size_t	ft_specifier_len(const char *format);
+t_flags	*ft_parse_specifier(const char *format);
 
 #endif
