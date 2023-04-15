b6ae715e777fefba82c15f807cf7cfde05b2879f
abdelbenamara <abdelb.contact@gmail.com>
2023-04-15 14:06:07 +0100

    Updated ft_vdd_conversion

--- conversion/ft_vdd_conversion.c
+++ conversion/ft_vdd_conversion.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_put_d_conversion_fd.c                           :+:      :+:    :+:   */
+/*   ft_vdd_conversion.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:02:05 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/01 18:45:21 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/15 13:03:43 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "conversion/libftprintf_conversion.h"
+#include "libftprintf_conversion.h"
 
-int	ft_put_d_conversion_fd(const char *format, va_list *ap, int fd)
+int	ft_vdd_conversion(int fd, t_flags *flags, va_list *ap)
 {
 	int		nb;
 	int		i;
 	char	*a;
 
-	(void)format;
 	i = va_arg(*ap, int);
 	a = ft_itoa(i);
 	i = 0;
@@ -29,5 +28,6 @@ int	ft_put_d_conversion_fd(const char *format, va_list *ap, int fd)
 	}
 	free(a);
 	nb = i;
+	free(flags);
 	return (nb);
 }
