9abba7735b9a8f5698838cb49bdb06f68a522061
abdelbenamara <abdelb.contact@gmail.com>
2023-04-15 14:05:46 +0100

    Updated ft_vdc_conversion

--- conversion/ft_vdc_conversion.c
+++ conversion/ft_vdc_conversion.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_put_c_conversion_fd.c                           :+:      :+:    :+:   */
+/*   ft_vdc_conversion.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 13:35:57 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/01 18:45:06 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/15 13:03:37 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "conversion/libftprintf_conversion.h"
+#include "libftprintf_conversion.h"
 
-int	ft_put_c_conversion_fd(const char *format, va_list *ap, int fd)
+int	ft_vdc_conversion(int fd, t_flags *flags, va_list *ap)
 {
 	int		nb;
 	char	c;
 
-	(void)format;
 	nb = 0;
 	c = va_arg(*ap, int);
 	ft_putchar_fd(c, fd);
 	++nb;
+	free(flags);
 	return (nb);
 }
