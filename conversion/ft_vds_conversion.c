c4d163ce768b695715fb0d1bfb9f3d6cec0317b2
abdelbenamara <abdelb.contact@gmail.com>
2023-04-15 14:07:16 +0100

    Updated ft_vds_conversion

--- conversion/ft_vds_conversion.c
+++ conversion/ft_vds_conversion.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_put_s_conversion_fd.c                           :+:      :+:    :+:   */
+/*   ft_vds_conversion.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 15:58:19 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/01 23:09:35 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/15 13:04:00 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "conversion/libftprintf_conversion.h"
+#include "libftprintf_conversion.h"
 
-int	ft_put_s_conversion_fd(const char *format, va_list *ap, int fd)
+int	ft_vds_conversion(int fd, t_flags *flags, va_list *ap)
 {
 	int		nb;
 	char	*s;
 
-	(void)format;
 	s = va_arg(*ap, char *);
 	if (!s)
 	{
@@ -34,5 +33,6 @@ int	ft_put_s_conversion_fd(const char *format, va_list *ap, int fd)
 			++nb;
 		}
 	}
+	free(flags);
 	return (nb);
 }
