5f51f70b3b951bd2470d3da5f7a98da043477d04
abdelbenamara <abdelb.contact@gmail.com>
2023-04-16 15:39:36 +0100

    Updated ft_vdi_conversion

--- conversion/ft_vdi_conversion.c
+++ conversion/ft_vdi_conversion.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:02:19 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/15 13:03:46 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/16 14:14:07 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf_conversion.h"
 
-int	ft_vdi_conversion(int fd, t_flags *flags, va_list *ap)
+int	ft_vdi_conversion(int fd, t_cslpf *flags, va_list *ap)
 {
 	return (ft_vdd_conversion(fd, flags, ap));
 }
