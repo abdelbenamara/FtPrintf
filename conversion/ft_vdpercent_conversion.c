d08d8135801f1cc886b8957fd070a2057ae4d04f
abdelbenamara <abdelb.contact@gmail.com>
2023-04-16 15:39:56 +0100

    Updated ft_vdpercent_conversion

--- conversion/ft_vdpercent_conversion.c
+++ conversion/ft_vdpercent_conversion.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:03:41 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/15 13:03:58 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/16 14:14:18 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf_conversion.h"
 
-int	ft_vdpercent_conversion(int fd, t_flags *flags, va_list *ap)
+int	ft_vdpercent_conversion(int fd, t_cslpf *flags, va_list *ap)
 {
 	(void)ap;
 	ft_putchar_fd('%', fd);
