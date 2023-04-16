c276508ec9e258ef82f12f8c2afb6bbf625df141
abdelbenamara <abdelb.contact@gmail.com>
2023-04-16 15:40:22 +0100

    Updated ft_vdu_conversion

--- conversion/ft_vdu_conversion.c
+++ conversion/ft_vdu_conversion.c
@@ -6,14 +6,14 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:02:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/15 13:04:06 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/16 14:14:23 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf_utils.h"
 #include "libftprintf_conversion.h"
 
-int	ft_vdu_conversion(int fd, t_flags *flags, va_list *ap)
+int	ft_vdu_conversion(int fd, t_cslpf *flags, va_list *ap)
 {
 	int				nb;
 	unsigned int	u;
