872f96a2831bd5e75a7cdc353e29ac7337c6c7ef
abdelbenamara <abdelb.contact@gmail.com>
2023-04-16 15:38:50 +0100

    Added ft_parse_flags

--- conversion/specifiers/ft_parse_flags.c
+++ conversion/specifiers/ft_parse_flags.c
@@ -1,23 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_specifier_len.c                                 :+:      :+:    :+:   */
+/*   ft_parse_flags.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 11:03:35 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/15 12:11:35 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/16 14:21:24 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conversion/specifiers/libftprintf_conversion_specifiers.h"
 
-size_t	ft_specifier_len(const char *format)
+void	ft_parse_flags(const char c, t_cslpf *flags)
 {
-	size_t	i;
-
-	i = 1;
-	while (format[i] && !ft_is_specifier(format[i]) && !ft_isalpha(format[i]))
-		++i;
-	return (i);
+	if (c == '#')
+		flags->alternate_form = 1;
 }
