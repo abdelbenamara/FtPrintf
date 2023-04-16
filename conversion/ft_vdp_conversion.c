54d14b6e34f3f4bbe8740471abb8e066dc6d3854
abdelbenamara <abdelb.contact@gmail.com>
2023-04-16 15:39:51 +0100

    Updated ft_vdp_conversion

--- conversion/ft_vdp_conversion.c
+++ conversion/ft_vdp_conversion.c
@@ -6,14 +6,14 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:00:40 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/15 13:03:53 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/04/16 14:14:15 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf_utils.h"
 #include "libftprintf_conversion.h"
 
-int	ft_vdp_conversion(int fd, t_flags *flags, va_list *ap)
+int	ft_vdp_conversion(int fd, t_cslpf *flags, va_list *ap)
 {
 	int		nb;
 	void	*p;
