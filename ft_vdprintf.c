c85e99d647ece08d8309b94bc9fef5bbf0a27127
Anes BENAMARA <abenamar@student.42.fr>
2023-05-01 17:29:13 +0200

    Updated ft_vdprintf

--- ft_vdprintf.c
+++ ft_vdprintf.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 01:02:55 by abenamar          #+#    #+#             */
-/*   Updated: 2023/04/30 13:30:28 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/05/01 17:04:56 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_vdprintf(int fd, const char *format, va_list ap)
 		if (*format != '%')
 			(ft_putchar_fd(*format, fd), ++nb);
 		else
-			nb += ft_vdconvert(fd, &format, &ap);
+			nb += ft_vdconvert(fd, &format, ap);
 		++format;
 	}
 	return (nb);
